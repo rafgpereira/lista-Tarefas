c6e4cc01f368e929b5453849c5432e66210e645b
rafgpereira <rafaelgomespereira123@gmail.com>
2023-12-27 23:01:21 -0300

    local storage update and new readme

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,72 +2,73 @@ const inputTarefa = document.querySelector("input");
 const botaoAdicionar = document.querySelector(".botaoAdicionar");
 const listaTarefas = document.querySelector(".listaTarefas");
 
-function criaLi(){
-    const li = document.createElement('li')
-    return li
+function criaLi() {
+  const li = document.createElement("li");
+  return li;
 }
 
-function limpaInput(){
-    inputTarefa.value=''
-    inputTarefa.focus()
+function limpaInput() {
+  inputTarefa.value = "";
+  inputTarefa.focus();
 }
 
-function criaBotaoApagar(li){
-    li.innerText+= "  "
-    const botaoApagar = document.createElement('button')
-    botaoApagar.innerText = ' Apagar '
-    botaoApagar.setAttribute('class', 'apagar')
-    li.appendChild(botaoApagar)
+function criaBotaoApagar(li) {
+  li.innerText += "  ";
+  const botaoApagar = document.createElement("button");
+  botaoApagar.innerText = " Apagar ";
+  botaoApagar.setAttribute("class", "apagar");
+  li.appendChild(botaoApagar);
 }
 
-function salvarTarefas(){
-    const liTarefas = listaTarefas.querySelectorAll('li')
-    const vetorDeTarefas = []
+function salvarTarefas() {
+  const liTarefas = listaTarefas.querySelectorAll("li");
+  const vetorDeTarefas = [];
 
-    for(let tarefa of liTarefas){
-        let tarefaTexto = tarefa.innerText
-        tarefaTexto = tarefaTexto.replace('Apagar', '')
-        vetorDeTarefas.push(tarefaTexto)
-    }
-    const tarefasJSON = JSON.stringify(vetorDeTarefas)
-    localStorage.setItem('tarefas', tarefasJSON)
+  for (let tarefa of liTarefas) {
+    let tarefaTexto = tarefa.innerText;
+    tarefaTexto = tarefaTexto.replace("Apagar", "");
+    vetorDeTarefas.push(tarefaTexto);
+  }
+  const tarefasJSON = JSON.stringify(vetorDeTarefas);
+  localStorage.setItem("tarefas", tarefasJSON);
 }
-function criarTarefa(textoInput){
-    const li = criaLi()
-    li.innerText = textoInput
-    listaTarefas.appendChild(li)
-    limpaInput()
-    criaBotaoApagar(li)
-    salvarTarefas()
-}
-
-function recuperaTarefasSalvas(){
-    const tarefas = localStorage.getItem('tarefas')
-    const listaDeTarefas = JSON.parse(tarefas)
-    for(let tarefa of listaDeTarefas){
-        criarTarefa(tarefa)
-    }
+function criarTarefa(textoInput) {
+  const li = criaLi();
+  li.innerText = textoInput;
+  listaTarefas.appendChild(li);
+  limpaInput();
+  criaBotaoApagar(li);
+  salvarTarefas();
 }
 
-recuperaTarefasSalvas()
+function recuperaTarefasSalvas() {
+  const tarefas = localStorage.getItem("tarefas");
+  const listaDeTarefas = JSON.parse(tarefas);
+  for (let tarefa of listaDeTarefas) {
+    criarTarefa(tarefa);
+  }
+}
 
-inputTarefa.addEventListener('keypress', (e)=>{
-    if(e.key == "Enter"){
-        if (!inputTarefa.value) return
-        criarTarefa(inputTarefa.value)
-    }
-})
+if (localStorage.getItem("tarefas")) {
+  recuperaTarefasSalvas();
+}
 
-botaoAdicionar.addEventListener("click", () => {
-    if (!inputTarefa.value) return
-    criarTarefa(inputTarefa.value)
+inputTarefa.addEventListener("keypress", (e) => {
+  if (e.key == "Enter") {
+    if (!inputTarefa.value) return;
+    criarTarefa(inputTarefa.value);
+  }
 });
 
-document.addEventListener('click', (e)=>{
-    const elemento = e.target
-    if(elemento.classList.contains('apagar')){
-        elemento.parentElement.remove()
-        salvarTarefas()
-    }
+botaoAdicionar.addEventListener("click", () => {
+  if (!inputTarefa.value) return;
+  criarTarefa(inputTarefa.value);
+});
 
-})
+document.addEventListener("click", (e) => {
+  const elemento = e.target;
+  if (elemento.classList.contains("apagar")) {
+    elemento.parentElement.remove();
+    salvarTarefas();
+  }
+});
